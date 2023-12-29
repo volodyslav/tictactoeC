777d3520bd595e6cb4e9b79aa9bf22f63955b445
Volodyslav <122315136+volodyslav@users.noreply.github.com>
2023-12-29 13:27:03 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+char combination(char place[])
+{
+    for (int i = 0; i < 9; i += 3)
+    {
+        if (place[i] == place[i + 1] && place[i + 1] == place[i + 2] && place[i] != ' ')
+        {
+            printf("Win %c", place[i]);
+        }
+    }
+    for (int j = 0; j < 9; j += 3)
+    {
+        if (place[j] == place[j + 3] && place[j + 3] == place[j + 6] && place[j] != ' ')
+        {
+            printf("Win %c", place[j]);
+        }
+    }
+    if ((place[0] == place[4] && place[4] == place[8] && place[0] != ' ') ||
+        (place[2] == place[4] && place[4] == place[6] && place[2] != ' '))
+    {
+        printf("Win %c", place[4]);
+    }
+}
 int randomAI(char array[], int size);
 int main()
 {
     int numSum = 0;
-    char player = 'x';
-    char ai = 'o';
+    char player = 'X';
+    char ai = 'O';
     char placeNumber[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 
     do
     {
         int number;
         scanf("%d", &number);
-        printf("%d", number);
-        int random = randomAI(placeNumber, 9);
+        printf("Number %d", number);
 
         if (number >= 0 && number < 9)
         {
@@ -25,6 +46,8 @@ int main()
             else
             {
                 placeNumber[number] = player;
+                int random = randomAI(placeNumber, 9);
+
                 if (placeNumber[random] != ' ')
                 {
                     random = randomAI(placeNumber, 9);
@@ -33,7 +56,6 @@ int main()
                 {
                     placeNumber[random] = ai;
                 }
-
                 numSum++;
                 printf("\n");
                 printf(" %c | %c | %c\n", placeNumber[0], placeNumber[1], placeNumber[2]);
@@ -42,6 +64,7 @@ int main()
                 printf("-----------\n");
                 printf(" %c | %c | %c\n", placeNumber[6], placeNumber[7], placeNumber[8]);
                 printf("\n");
+                combination(placeNumber);
             }
         }
 
@@ -57,6 +80,19 @@ int main()
 int randomAI(char array[], int size)
 {
     srand(time(NULL));
-    int random = rand() % 9;
+    int random;
+    int randomCount = 0;
+    do
+    {
+        random = rand() % 9;
+        randomCount++;
+        if (randomCount > 8)
+        {
+            break;
+        }
+        printf("\nRandom %d", random);
+
+    } while (array[random] != ' ');
+
     return random;
 }
